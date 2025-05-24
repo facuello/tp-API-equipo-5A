8c66c47066c3ffacfdd24439b64797202c864152
Juan Manuel Fernández Bogo <144643059+JuanManuelFernandez@users.noreply.github.com>
2025-05-24 16:14:51 -0300

    Tercer Commit
    Agregue la función de listar y agregar elementos, las validaciones las voy a hacer luego.

--- api-productos/Controllers/ArticuloController.cs
+++ api-productos/Controllers/ArticuloController.cs
@@ -6,52 +6,54 @@ using System.Net.Http;
 using System.Web.Http;
 using Articulos;
 using BaseDeDatos;
-using api_productos.Models
-
+using api_productos.Models;
+using Categorias;
 
 namespace api_productos.Controllers
 {
     public class ArticuloController : ApiController
     {
-        // GET: api/Articulo
+
+        //GET: api/Articulo
         public IEnumerable<Articulo> Get()
         {
-            Catalogo catalogo = new Catalogo();
+            CatalogoArticulo catalogo = new CatalogoArticulo();
 
-            //return catalogo.listar();
+            return catalogo.listar();
         }
 
         // GET: api/Articulo/5
-        public Articulo Get(int id)
-        {
-            Catalogo catalogo = new Catalogo();
-            //List<Articulo> lista = catalogo.listar();
+        //public Articulo Get(int id)
+        //{
+        //    Catalogo catalogo = new Catalogo();
+        //    //List<Articulo> lista = catalogo.listar();
 
-            //return lista.Find(x=> x.Id == id);
-        }
+        //    //return lista.Find(x=> x.Id == id);
+        //}
 
         // POST: api/Articulo
-        public void Post([FromBody]ArticuloDto art)
+        public void Post([FromBody] ArticuloDto art)
         {
             CatalogoArticulo catalogo = new CatalogoArticulo();
             Articulo nuevo = new Articulo();
             nuevo.Codigo = art.Codigo;
             nuevo.Nombre = art.Nombre;
             nuevo.Descripcion = art.Descripcion;
-            nuevo.Marc = new Marca { Id = art.IdMarca};
-            //nuevo.Categ = new Categoria{ Id = art.IdCategoria }; No me toma la clase Categoria de Articulo :/
+            nuevo.Marc = new Marca { Id = art.IdMarca };
+            nuevo.Categ = new Categoria { Id = art.IdCategoria };
             nuevo.Imagen = art.Imagen;
             nuevo.Precio = art.Precio;
 
             catalogo.agregarArticulo(nuevo);
-        public void Post([FromBody]Articulo value)
-        {
         }
+        //public void Post([FromBody] Articulo value)
+        //{
+        //}
 
         // PUT: api/Articulo/5
-        public void Put(int id, [FromBody]Articulo value)
-        {
-        }
+        //public void Put(int id, [FromBody] Articulo value)
+        //{
+        //}
 
         // DELETE: api/Articulo/5
         public void Delete(int id)
